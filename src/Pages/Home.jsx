5d33189c6133e126b52145ebccbeda30d601f50f
Artem Bakum <artem.bakum@mail.ru>
2025-05-12 22:14:02 +0300

    added routing

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Categories } from "../components/Categories";
+import { PizzaBlock } from "../components/PizzaBlock";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+import { Sort } from "../components/Sort";
+
+
+
+export const Home = () => {
+
+const [categoryActiveIndex, setCategoryActiveIndex] = React.useState(0)
+
+  const [items, setItems] = React.useState([0]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  React.useEffect(() => {
+    fetch(`https://67877bf5c4a42c916106edd2.mockapi.io/items?${categoryActiveIndex !== 0 ? `category=${categoryActiveIndex}`: '' }` )
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, [categoryActiveIndex]);
+  return (
+    <>
+        <div className="content__top">
+          <Categories />
+          <Sort />
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
+          {isLoading
+            ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+            : items.map((obj, index) => <PizzaBlock key={index} {...obj} categoryIndex={categoryActiveIndex} setCategoryIndex= {(i) => setCategoryActiveIndex(i)}/>) 
+          }
+        </div>
+        </>
+   
+  );
+};
