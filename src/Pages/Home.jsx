f30faca5b1e4d35793d281d58904898ef99feaa4
Artem Bakum <artem.bakum@mail.ru>
2025-05-13 10:31:57 +0300

    set up filtering

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,53 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Categories } from "../components/Categories";
 import { PizzaBlock } from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import { Sort } from "../components/Sort";
 
-<<<<<<< HEAD
 
 
 export const Home = () => {
 
-const [categoryActiveIndex, setCategoryActiveIndex] = React.useState(0)
-
+  const [categoryIndex, setCategoryIndex] = useState(0)
   const [items, setItems] = React.useState([0]);
   const [isLoading, setIsLoading] = React.useState(true);
   React.useEffect(() => {
-    fetch(`https://67877bf5c4a42c916106edd2.mockapi.io/items?${categoryActiveIndex !== 0 ? `category=${categoryActiveIndex}`: '' }` )
-=======
-export const Home = () => {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-  React.useEffect(() => {
-    fetch("https://67877bf5c4a42c916106edd2.mockapi.io/items")
->>>>>>> 6f2951a7d5cf43af966180a7f100bd6e7ba3d367
+    fetch(`https://67877bf5c4a42c916106edd2.mockapi.io/items?${categoryIndex > 0 ? `category=${categoryIndex}` : ''} ` )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
-<<<<<<< HEAD
-  }, [categoryActiveIndex]);
-=======
-  }, []);
->>>>>>> 6f2951a7d5cf43af966180a7f100bd6e7ba3d367
+  }, [categoryIndex]);
   return (
     <>
         <div className="content__top">
-          <Categories />
+          <Categories categoryIndex={categoryIndex} setCategoryIndex={(i) => setCategoryIndex(i)} />
           <Sort />
         </div>
         <h2 className="content__title">Все пиццы</h2>
         <div className="content__items">
           {isLoading
             ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-<<<<<<< HEAD
-            : items.map((obj, index) => <PizzaBlock key={index} {...obj} categoryIndex={categoryActiveIndex} setCategoryIndex= {(i) => setCategoryActiveIndex(i)}/>) 
+            : items.map((obj, index) => <PizzaBlock key={index} {...obj} />) 
           }
-=======
-            : items.map((obj, index) => <PizzaBlock key={index} {...obj} />)}
->>>>>>> 6f2951a7d5cf43af966180a7f100bd6e7ba3d367
         </div>
         </>
    
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
-export const Categories = () => {
-  const [categoryActiveIndex, setCategoryActiveIndex] = React.useState(0)
+export const Categories = ({categoryIndex, setCategoryIndex}) => {
 
   const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
 
   return (
     <div className="categories">
     <ul>
-      {categories.map((cat, index) =>  <li onClick={()=>setCategoryActiveIndex(index)} className={index === categoryActiveIndex?'active':''} key={cat}>{cat}</li>)}
+      {categories.map((cat, index) =>  <li onClick={()=>setCategoryIndex(index)} className={index === categoryIndex?'active':''} key={cat}>{cat}</li>)}
     </ul>
   </div>
   )
